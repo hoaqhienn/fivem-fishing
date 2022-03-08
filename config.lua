1f131712442f3d10a010269f856115c60b65ac29
Davi Right Here <87158643+hoaqhienn@users.noreply.github.com>
2022-03-08 15:18:20 +0700

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,84 @@
+Config = {}
+
+Config.Debug = false
+Config.JobBusy = false
+
+Config.MarkerData = {
+    ["type"] = 6,
+    ["size"] = 2.0, 2.0, 2.0,
+    ["color"] = 0, 255, 150
+}
+
+Config.FishingRestaurant = {
+    ["name"] = "Sell Fish",
+    ["blip"] = {
+        ["sprite"] = 628,
+        ["color"] = 3
+    },
+    ["ped"] = {
+        ["model"] = 0xED0CE4C6,
+        ["position"] = vector3(-1038.4545898438, -1397.0551757813, 5.553192615509),
+        ["heading"] = 200.0
+    }
+}
+
+Config.FishingItems = {
+    ["rod"] = {
+        ["name"] = "fishingrod",
+        ["label"] = "Fishing Rod"
+    },
+    ["bait"] = {
+        ["name"] = "fishingbait",
+        ["label"] = "Fishing Bait"
+    },
+    ["goldfish"] = {
+        ["price"] = 25
+    },
+    ["fish"] = {
+        ["price"] = 50 
+    },
+    ["bluefish"] = {
+        ["price"] = 100
+    },
+    ["redfish"] = {
+        ["price"] = 150
+    },
+    ["stripedbass"] = {
+        ["price"] = 300
+    },
+    ["pacifichalibut"] = {
+        ["price"] = 350
+    },
+    ["largemouthbass"] = {
+        ["price"] = 400
+    },
+    ["salmon"] = {
+        ["price"] = 450
+    },
+    ["catfish"] = {
+        ["price"] = 500
+    },
+    ["tigersharkmeat"] = {
+        ["price"] = 600
+    },
+    ["stingraymeat"] = {
+        ["price"] = 700
+    },
+    ["killerwhalemeat"] = {
+        ["price"] = 800
+    },
+}
+
+Config.FishingZones = {
+    {
+        ["name"] = "Beach Fishing",
+        ["coords"] = vector3(-1948.1279296875, -749.79125976563, 2.5400819778442),
+        ["radius"] = 500.0,
+    },
+    {
+        ["name"] = "special0",
+        ["coords"] = vector3(7040.34, 8172.63, 204.435),
+        ["radius"] = 500.0,
+        ["secret"] = true,
+    },
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+client_scripts {
+	"client/functions.lua",
+	"client/main.lua"
+}
+
+server_scripts {
+	"server/main.lua"
+}
+
+shared_scripts {
+	"config.lua"
+}
+
